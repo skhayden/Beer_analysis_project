d3bf22b0e4bc5319a011feeab53d2f42702060de
skhayden <31876525+skhayden@users.noreply.github.com>
2017-10-09 20:06:58 -0700

    Initial commit

---
+++ BeerResearchMakeFile.R
@@ -0,0 +1,12 @@
+#title: "HomeWork 4"
+#author: "Steven Hayden"
+#date: "Oct 8th, 2017"
+
+#Calls File to import data and clean it.
+source("Beer_data_Merge")
+
+#Calls File to analyze data
+source("")
+
+#Calls File to present data. 
+source("")
